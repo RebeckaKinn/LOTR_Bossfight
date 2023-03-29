c8f179789325b9d44f4ac87ac0f11b3f58631c8d
RebeckaKinn <rebeckaki@getacademy.no>
2023-03-29 13:38:07 +0200

    Add project files.

---
+++ Character.cs
@@ -0,0 +1,21 @@
+﻿namespace Bossfight
+{
+    internal class Character
+    {
+        private string Name { get; }
+        private string Status { get; }
+        private int Health { get; }
+        private int Strength { get; }
+        private int Stamina { get; }
+
+        public Character(string name, string status, int health, int strength, int stamina)
+        {
+            Name = name;
+            Status = status;
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+        }
+
+    }
+}
---
+++ CharacterList.cs
@@ -0,0 +1,17 @@
+﻿namespace Bossfight
+{
+    public class CharacterList
+    {
+        private List<Character> ListOfCharacters { get; set; }
+
+        public CharacterList()
+        {
+            ListOfCharacters = new List<Character>
+            {
+                new Character("Gandalf", "Hero", 100, 20, 40),
+                new Character("Balrog", "Enemy", 400, 20, 10)
+            };
+        }
+
+    }
+}
